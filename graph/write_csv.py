8a9595d6080b8882b2464b48ecfeb2de719adcf3
Pablo <pablo.rey@orange.fr>
2025-04-05 19:24:55 +0200

    added async

--- graph/write_csv.py
+++ graph/write_csv.py
@@ -1,4 +1,8 @@
 from pg_reddit_driver import RedditDB
+import pandas
+
+db = RedditDB()
 
 users = db.get_users()
 
+vertices = pandas.DataFrame({"id": [u[0] for u in users], "label": [u[1] for u in users]})
