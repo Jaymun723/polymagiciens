19eefa06dfcb59d9bb31177de5e5df92fc6ff39d
Victor CARO <victor.caro@polytechnique.edu>
2025-04-05 23:46:19 +0200

    Added scrapping

---
+++ graph/scrap.py
@@ -0,0 +1,25 @@
+from graph.reddit import RedditWrapper
+import praw
+from graph.pg_reddit_driver import RedditDB
+from graph.threading import ThreadedScraper
+
+
+def scrap():
+    db = RedditDB()
+    reddit = praw.Reddit("bot1", user_agent="polymagiciens bot1")
+    wrapper = RedditWrapper(db, reddit)
+
+    sub = reddit.subreddit("news")
+
+    def save_post(p):
+        try:
+            wrapper.treat_submission(p, 10)
+        except:
+            pass
+
+    scrapper = ThreadedScraper(save_post, 5)
+
+    for p in sub.top(time_filter="week", limit=50):
+        scrapper.process_post(p)
+
+    scrapper.wait_all()
---
+++ graph/threading.py
@@ -0,0 +1,52 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+
+class ThreadedScraper:
+    def __init__(self, scrape_func, max_workers=5):
+        """
+        Initialize the scraper with a scraping function and a maximum number of worker threads.
+
+        :param scrape_func: The function to scrape a post given its ID.
+        :param max_workers: Maximum number of threads to use concurrently.
+        """
+        self.scrape_func = scrape_func
+        self.executor = ThreadPoolExecutor(max_workers=max_workers)
+        self.futures = []
+
+    def process_post(self, post_id):
+        """
+        Submit the scraping task for a given post ID to the thread pool.
+
+        :param post_id: The ID of the post to scrape.
+        :return: A Future object representing the result of the task.
+        """
+        future = self.executor.submit(self.scrape_func, post_id)
+        self.futures.append(future)
+        return future
+
+    def wait_all(self):
+        """
+        Wait for all submitted tasks to complete and return their results.
+
+        :return: A list of results from all scraping tasks in the order they were submitted.
+        """
+        results = []
+        for future in self.futures:
+            results.append(future.result())
+        self.futures.clear()  # Clear the futures list after retrieving results
+        return results
+
+    def shutdown(self, wait=True):
+        """
+        Shutdown the executor and free resources.
+
+        :param wait: If True, wait for all pending tasks to complete before shutting down.
+        """
+        self.executor.shutdown(wait=wait)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.shutdown(wait=True)
+        return False
