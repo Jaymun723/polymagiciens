fd248ef0b053e7cfca832fd967d02e3d29723668
Victor CARO <victor.caro@polytechnique.edu>
2025-04-05 19:07:39 +0200

    Score attribution

--- graph/attribute_score.py
+++ graph/attribute_score.py
@@ -1,4 +1,5 @@
 from graph.pg_reddit_driver import RedditDB
+from graph.reddit import scrap_post
 from llms import compute_post_score, is_post_intresting
 
 N = 4
@@ -12,3 +13,4 @@ if __name__ == "__main__":
         if is_post_intresting(post.title, post.content, post.date):
             score = compute_post_score(post)
             db.update_post_score(post.id, score)
+            scrap_post(post.id)
