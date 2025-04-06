6638eec1a95061f2504ff9fb44b4234a1c20fa31
Victor CARO <victor.caro@polytechnique.edu>
2025-04-06 05:57:05 +0200

    Multi-threaded comment score

--- scripts/comment_score.py
+++ scripts/comment_score.py
@@ -1,18 +1,19 @@
 from graph.pg_reddit_driver import RedditDB
 from llms.match_score.comment_nli import comment_score as multiplier_score
 from graph.reddit import RedditWrapper
+from graph.threading_scrapper import ThreadedScraper
 import praw
 
 
 def comment_score():
     db = RedditDB()
     reddit = praw.Reddit("bot1", user_agent="polymagiciens bot1")
+    wrapper = RedditWrapper(db, reddit)
 
     posts = db.get_treated_posts_ordered_by_comments()
     print(f"There are {len(posts)} to deal with.")
 
-    for p in posts:
-        wrapper = RedditWrapper(db, reddit)
+    def process_fn(p):
         wrapper.treat_submission(reddit.submission(p[0]), 1, save_post=False)
 
         comments = db.get_comments_by_post_id(p[0])
@@ -24,3 +25,10 @@ def comment_score():
             scaled_score = multiplier * (p[6] / 100)  # score
             score = int(scaled_score * 50 + 50)
             db.update_comment_score(comment[0], score)
+
+    scrapper = ThreadedScraper(process_fn, max_workers=32)
+
+    for p in posts:
+       scrapper.process_post(p)
+
+    scrapper.wait_all()
