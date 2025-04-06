411cb2f7186dba7b1cdc463b07a64f95060082cb
Victor CARO <victor.caro@polytechnique.edu>
2025-04-06 10:41:23 +0200

    Now computes only scores

--- scripts/comment_score.py
+++ scripts/comment_score.py
@@ -8,14 +8,14 @@ def comment_score():
     from llms.match_score.comment_nli import comment_score as multiplier_score
 
     db = RedditDB()
-    reddit = praw.Reddit("bot1", user_agent="polymagiciens bot1")
-    wrapper = RedditWrapper(db, reddit)
+    # reddit = praw.Reddit("bot1", user_agent="polymagiciens bot1")
+    # wrapper = RedditWrapper(db, reddit)
 
     posts = db.get_treated_posts_ordered_by_comments()
     print(f"There are {len(posts)} to deal with.")
 
     def process_fn(p):
-        wrapper.treat_submission(reddit.submission(p[0]), 1, save_post=False)
+        # wrapper.treat_submission(reddit.submission(p[0]), 1, save_post=False)
 
         comments = db.get_comments_by_post_id(p[0])
         print(f"Post {p[0]} has {len(comments)} attached.")
